38dcffcd51f5f7cb3c3667a055cca37b62e0b047
Buney <dpkinser93@gmail.com>
2021-06-23 20:40:46 -0600

    text generation overhaul

--- index.js
+++ index.js
@@ -2,43 +2,14 @@
 const inquirer = require('inquirer');
 const fileSystem = require('fs');
 
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'name',
+        name: 'title',
         message: 'What is your application called?'
-    },
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'Write a brief description of your application.'
-    },
-    {
-        type: 'input',
-        name: 'Installation',
-        message: 'How can someone install the application?'
-    },
-    {
-        type: 'input',
-        name: 'Usage',
-        message: 'How does someone use your application?'
-    },
-    {
-        type: 'input',
-        name: 'Contributing',
-        message: `Can other users contribute to your project? How?.`
-    },
-    {
-        type: 'input',
-        name: 'Tests',
-        message: `How can users can test your application?`
-    },
-    {
-        type: 'list',
-        name: 'License',
-        message: `Which of these licenses would you like for your project?`,
-        choices: [ "MIT", "GNU GPLv2", "GNU GPLv3", "Apache 2.0", "ISC", "No license, thanks"]
     },
     {
         type: 'input',
@@ -49,27 +20,55 @@ const questions = [
         type: 'input',
         name: 'email',
         message: `What is your email address?`
-    } 
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: `Which of these licenses would you like for your project?`,
+        choices: [ "MIT", "GNU GPLv2", "GNU GPLv3", "Apache 2.0", "ISC", "No license, thanks"]
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a brief description of your application.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How can someone install the application?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How does someone use your application?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: `Can other users contribute to your project? How?.`
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: `How can users can test your application?`
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const keys = Object.keys(data);
-    console.log(keys);
-    fileSystem.writeFile(`${fileName}.md`, `# ${data.name}\n\n`, err => 
+
+    // Create a .md file with the app name as the title
+    fileSystem.writeFile(`${fileName.replace(' ', '').toUpperCase()}.md`, data, err => 
         err ? console.log(err) : console.log(`Document created!`));
-    keys.forEach(entry => {
-        fileSystem.appendFile(`${fileName}.md`, `## ${entry}\n${data[`${entry}`]}\n\n`, err =>
-        err ? console.log(err) : console.log(`${entry} section added!`));
-    });
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then(data => {
-        writeToFile(data.name, data);
+        writeToFile(`${data.title}`, generateMarkdown(data))
     })
+
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "GNU GPLv2":
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)";
+    case "GNU GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)";
+    case "Apache 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "ISC":
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`";
+    case "GNU GPLv2":
+      return "`[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`";
+    case "GNU GPLv3":
+      return "`[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)`";
+    case "Apache 2.0":
+      return "`[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)`";
+    case "ISC":
+      return "`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`";
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data.license);
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}\n
+${renderLicenseLink(data.license)}\n
 
 `;
 }
